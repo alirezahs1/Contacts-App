2d88ca798e924de5f26ac4bff79fee5b1ba77102
Alireza Hooshyar <hooshyar.net@gmail.com>
2022-07-03 18:04:43 +0430

    fixbug: fix wrong variable name API_BASE

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 export const ITEMS_PER_PAGE = 20;
 
-export const API_BASE = process.env.BASE_URL || "http://localhost:1337";
+export const API_BASE = process.env.API_BASE || "http://localhost:1337";
